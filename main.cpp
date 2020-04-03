4360301c9fcccb7478d54f63fae269f23a073886
STenLYMARSH <63102493+STenLYMARSH@users.noreply.github.com>
2020-04-03 16:25:54 +0300

    Create new MAIN()_CPP
    Создан файл cpp содержащий главную функцию main() - с библиотеками: #include <iostream>
    #include <conio.h>
    #include <time.h>
    #include <random> в главном файле

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include <time.h>
+#include <random>
+//------------------
+using namespace::std;
+//------------------
+
+int main()
+{
+    setlocale(LC_ALL , "RUSSIAN");
+
+   
+   _getch();
+   return 0;
+};
